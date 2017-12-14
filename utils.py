f3033b8cb21fa62431b10da242122dc218b0fa3b
Hanut <hanut.mathur@live.in>
2017-12-14 05:12:11 +0000

    Added function definitions to utility class

--- utils.py
+++ utils.py
@@ -1,1 +1,31 @@
 ## Utilities for parsing json, and other tasks to be carried out for every transaction
+
+import json
+
+from http.client import HTTPConnection
+
+
+def load_config(config,path):
+	## parse data from config files
+	pass
+
+def get_port(s):
+	## return tuple of host, port
+	pass
+
+def Locked(path, host, port):
+	## Check if file is locked
+	pass
+
+def LockFile(path, host, port):
+	## Lock file
+	pass
+
+def unLock(path, host, port):
+	## Unlock file 
+	pass
+
+def getServer(path, host, port):
+	## get server details
+	pass
+
