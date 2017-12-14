324cdb9ef4f03063b7b04a693314454eee6c49c3
Hanut <hanut.mathur@live.in>
2017-12-14 17:32:15 +0000

    Declare LockServer methods

--- LockServer.py
+++ LockServer.py
@@ -1,1 +1,51 @@
 ### Service for locking file access so that two clients cannot access a file together
+
+import atexit ## to implement lock closing
+import shelve
+
+class LockServer():
+	## Locks file access as per requirements
+
+
+	def GET():
+
+	pass
+
+	def POST():
+
+	pass
+
+	def DELETE():
+
+	pass
+
+def lockExpiry():
+
+	pass
+
+def newLock():
+
+	pass
+
+def checkNewLock():
+
+	pass
+
+def updateLock():
+
+	pass
+
+def revokeLock():
+
+	pass
+
+
+_config = { 'dbfile' : 'locks.db',
+	    'lock_lifetime' : 60
+	  }
+
+## loading config data
+
+_locks = shelve.open(_config['dbfile'])
+
+atexit.register(lambda: _locks.close())
